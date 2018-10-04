badc343bde0d45a25cb42408d77c3a185cc3c30d
shivam1708 <shivam.pawase@somaiya.edu>
2018-10-04 08:53:15 +0530

    new init

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-const shell = require('shelljs');
-
 const express = require('express');
 const bodyparser = require('body-parser');
-const fs = require('file-system');
 
 const app = express();
 app.use(bodyparser.json());
@@ -15,43 +12,30 @@ var total = 1000;
 var disabled = '';
 var flag = 1;
 
-var li = [];
-fs.readFile('./text/one.cpp','utf8',function(err,data){
-  if(err){
-    console.log(err);
-  }
-  else{
-    li = data.split('\n');
-    //console.log(li);
-  }
-});
+var li = ['code here'];
 
 app.get('/',function(req, res){
     res.render('pages/index',{list:li,total:total,disabled:disabled});
 });
 
 app.post('/',function(req,res){
-  var a = req.body.input;
+  var b = req.body.input;
   console.log(a);
-  fs.writeFile('./text/sample.txt',a, function(err) {
-    if(err)
-      console.log(err);
-    else{
-      shell.exec('./text/one.out < ./text/sample.txt > ./text/routput.txt');
-      shell.exec('./text/two.out < ./text/sample.txt > ./text/woutput.txt');
-      var ans = shell.exec('diff ./text/routput.txt ./text/woutput.txt');
-      if(ans.stdout == ''){
-        total = total - 50;
-        res.render('pages/index',{list:li,total:total,disabled:disabled});
-      }
-      else if(flag == 1){
-        total = total + 100;
-        flag = 0;
-        disabled = 'disabled';
-        res.render('pages/index',{list:li,total:total,disabled:disabled});
-      }
-    }
-  }); 
+  var a = parseInt(b,10);
+
+  wrong = a*a*a;
+  right = Math.abs(a*a*a);
+
+  if(wrong==right){
+    total = total - 50;
+    res.render('pages/index',{list:li,total:total,disabled:disabled});
+  }
+  else if(flag == 1){
+    total = total + 100;
+    flag = 0;
+    disabled = 'disabled';
+    res.render('pages/index',{list:li,total:total,disabled:disabled});
+  }
 });
 
 app.listen(8080, () => console.log(`Express server running on port 8080`));
