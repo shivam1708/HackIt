c06cd409ddd66004c5bb87df2a91bb59074b07ba
inishchith <inishchith@gmail.com>
2018-10-05 23:58:59 +0530

    initial() heroku npm

--- app.js
+++ app.js
@@ -38,4 +38,6 @@ app.post('/',function(req,res){
   }
 });
 
-app.listen(3000, () => console.log(`Express server running on port 3000`));
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Express server running on port Port ENV.`));
