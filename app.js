c08e9bd1457efe9b49d5dcdc70cc9e385f475c71
inishchith <inishchith@gmail.com>
2018-10-05 23:42:29 +0530

    initial heroku

--- app.js
+++ app.js
@@ -38,4 +38,4 @@ app.post('/',function(req,res){
   }
 });
 
-app.listen(8080, () => console.log(`Express server running on port 8080`));
+app.listen(3000, () => console.log(`Express server running on port 3000`));
